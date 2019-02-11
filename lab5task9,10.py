0a643edc71750c2f7df42146b7fb905328462c4f
AKP96 <ajithkumar12314@gmail.com>
2019-02-11 00:48:12 +0000

    Lab5 submission

---
+++ lab5task9,10.py
@@ -0,0 +1,15 @@
+ACCORDING TO THE ABOVE PROGRAM, THE FOLLWOING TABLE IS TRUE. BUT IF YOU SEE, A PARENT CLASS CANNOt BE CONSIDERED AS SUBCLASS OF ITSELF SO MAKE A NOTE OF IT. OTHERWISE THE BELOW ANSWER IS RIGHT FOR THE QUESTION ASKED. 
+↓ is a subclass of →	Vehicle	LandVehicle	TrackedVehicle
+Vehicle	True	False  	False
+LandVehicle	True	True  	False  
+TrackedVehicle	True	True	True  
+  
+
+
+Task 10
+
+↓ is instance of →	Vehicle	LandVehicle	TrackedVehicle
+vehicle  	True	False	False
+landvehicle  	True	True	False
+trackedvehicle  	True  	True  	True 
+
